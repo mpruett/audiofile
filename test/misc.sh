b1ddece3101f7e2789a483993cd5a01faa97cab3
Michael Pruett <mpruett@sgi.com>
2001-07-10 20:50:25 +0000

    misc.sh:   Cleaning up inappropriate language.   Remove test files once the test is over.
    BKrev: 3b4bcd01ZW2Z550D549Z4Pp6G531Yw

--- test/misc.sh
+++ test/misc.sh
@@ -1,9 +1,11 @@
-./miscwrite /tmp/motherfuck || exit
-./miscread /tmp/motherfuck > /tmp/misc.fuck || exit
+./miscwrite /tmp/misc.test || exit
+./miscread /tmp/misc.test > /tmp/misc.out || exit
 
-diff /tmp/misc.fuck - <<END || exit
+diff /tmp/misc.out - <<END || exit
 Miscellaneous 201, 19 bytes:
 1998 Michael Pruett
 Miscellaneous 203, 43 bytes:
 Michael Pruett's home-brew methamphetamines
 END
+
+rm -f /tmp/misc.test /tmp/misc.out
