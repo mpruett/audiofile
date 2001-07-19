cc815918450ff6125ca48b1554243cdbd47e2ee1
Michael Pruett <mpruett@sgi.com>
2001-07-19 09:40:00 +0000

    ChangeLog, format.c:   Add afSetChannelMatrix and afGetVirtualChannels.
    BKrev: 3b570d60x4oyzfNzxkh2XF8ZcL5Vdg

--- libaudiofile/format.c
+++ libaudiofile/format.c
@@ -335,3 +335,38 @@ int afSetVirtualRate (AFfilehandle file, int trackid, double rate)
 
 	return 0;
 }
+
+void afSetChannelMatrix (AFfilehandle file, int trackid, double* matrix)
+{
+	_Track	*track;
+
+	if (!_af_filehandle_ok(file))
+		return;
+
+	if ((track = _af_filehandle_get_track(file, trackid)) == NULL)
+		return;
+
+	if (track->channelMatrix != NULL)
+		free(track->channelMatrix);
+	track->channelMatrix = NULL;
+
+	if (matrix != NULL)
+	{
+		int	i, size;
+
+		size = track->v.channelCount * track->f.channelCount;
+
+		for (i = 0; i < size; i++)
+			track->channelMatrix[i] = matrix[i];
+	}
+}
+
+int afGetVirtualChannels (AFfilehandle file, int trackid)
+{
+	_Track	*track;
+
+	if ((track = _af_filehandle_get_track(file, trackid)) == NULL)
+		return -1;
+
+	return track->v.channelCount;
+}
