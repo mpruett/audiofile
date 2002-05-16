abbc39b3964862f771262b5f2f0479b6323a6ae4
Michael Pruett <mpruett@sgi.com>
2002-05-16 11:35:52 +0000

    openclose.c, ChangeLog:   Implement afOpenVirtualFile.
    BKrev: 3ce3fc083DWNZxS8YStBdNwZu28_uA

--- libaudiofile/openclose.c
+++ libaudiofile/openclose.c
@@ -265,6 +265,40 @@ AFfilehandle afOpenFile (const char *filename, const char *mode, AFfilesetup set
 	return filehandle;
 }
 
+AFfilehandle afOpenVirtualFile (AFvirtualfile *vfile, const char *mode,
+	AFfilesetup setup)
+{
+	AFfilehandle	filehandle;
+	int		access; 
+
+	if (vfile == NULL)
+	{
+		_af_error(AF_BAD_FILEHANDLE, "null virtual filehandle");
+		return AF_NULL_FILEHANDLE;
+	}
+
+	if (mode == NULL)
+	{
+		_af_error(AF_BAD_ACCMODE, "null access mode");
+		return AF_NULL_FILEHANDLE;
+	}
+
+	if (mode[0] == 'r')
+		access = _AF_READ_ACCESS;
+	else if (mode[0] == 'w')
+		access = _AF_WRITE_ACCESS;
+	else
+	{
+		_af_error(AF_BAD_ACCMODE, "unrecognized access mode '%s'", mode);
+		return AF_NULL_FILEHANDLE;
+	}
+
+	if (_afOpenFile(access, vfile, NULL, &filehandle, setup) != AF_SUCCEED)
+		af_fclose(vfile);
+
+	return filehandle;
+}
+
 static status _afOpenFile (int access, AFvirtualfile *vf, const char *filename,
 	AFfilehandle *file, AFfilesetup filesetup)
 {
