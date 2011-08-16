6dbd747a910623c872c30a9627d4e61911d168a8
Michael Pruett <michael@68k.org>
2011-08-16 00:45:27 -0700

    Remove obsolete Windows project files.

--- win32/build_all.bat
+++
@@ -1,4 +0,0 @@
-nmake -f sfinfo.mak CFG="sfinfo - Win32 Debug"
-nmake -f sfinfo.mak CFG="sfinfo - Win32 Release"
-nmake -f sfinfo.mak CFG="sfinfo - Win32 Debug using DLL"
-nmake -f sfinfo.mak CFG="sfinfo - Win32 Release using DLL"
