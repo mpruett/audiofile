807aaa6c3a3300cf1ddf535cc1fba78b51d71446
Michael Pruett <michael@68k.org>
2016-07-04 19:50:02 -0500

    Replace custom configuration script with autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -1,52 +1,19 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname $0`
+test -n "$srcdir" || srcdir=.
 
-DIE=0
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile libaudiofile."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.57.tar.gz"
-	echo "(or a newer version if it is available)."
-	DIE=1
-}
-
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile libaudiofile."
-	echo "Get ftp://ftp.gnu.org/gnu/libtool/libtool-1.4.2.tar.gz"
-	echo "(or a newer version if it is available)."
-	DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile libaudiofile."
-	echo "Get ftp://ftp.gnu.org/gnu/automake/automake-1.5.tar.gz"
-	echo "(or a newer version if it is available)."
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+	echo "autoreconf was not found; you must install automake, autoconf,"
+	echo "and libtool to build libaudiofile."
 	exit 1
-fi
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
 fi
 
 rootme=`pwd`
 cd $srcdir
-libtoolize --copy --force
-aclocal $ACLOCAL_FLAGS
-autoheader
-automake --add-missing
-autoconf
+autoreconf --install --verbose || exit $?
 cd $rootme
 
 $srcdir/configure "$@"
