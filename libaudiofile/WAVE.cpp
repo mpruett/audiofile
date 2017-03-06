c48e4c6503f7dabd41f11d4c9c7b7f8960e7f2c0
Antonio Larrosa <larrosa@kde.org>
2017-03-06 12:51:22 +0100

    Always check the number of coefficients
    When building the library with NDEBUG, asserts are eliminated
    so it's better to always check that the number of coefficients
    is inside the array range.
    
    This fixes the 00191-audiofile-indexoob issue in #41

--- libaudiofile/WAVE.cpp
+++ libaudiofile/WAVE.cpp
@@ -281,6 +281,12 @@ status WAVEFile::parseFormat(const Tag &id, uint32_t size)
 
 			/* numCoefficients should be at least 7. */
 			assert(numCoefficients >= 7 && numCoefficients <= 255);
+			if (numCoefficients < 7 || numCoefficients > 255)
+			{
+				_af_error(AF_BAD_HEADER,
+						"Bad number of coefficients");
+				return AF_FAIL;
+			}
 
 			m_msadpcmNumCoefficients = numCoefficients;
 
